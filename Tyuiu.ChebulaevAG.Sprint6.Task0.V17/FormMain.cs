a153880b3fffe53c257c2d9a86080a030fedf94d
Tyuiu.ChebulaevAG@outlook.com <andrey.chebulaev@gmail.com>
2023-12-15 19:23:36 +0500

    Sprint6

--- Tyuiu.ChebulaevAG.Sprint6.Task0.V17/FormMain.cs
+++ Tyuiu.ChebulaevAG.Sprint6.Task0.V17/FormMain.cs
@@ -19,14 +19,38 @@ namespace Tyuiu.ChebulaevAG.Sprint6.Task0.V17
             InitializeComponent();
         }
 
-        private void buttonDone_Click(object sender, EventArgs e)
+        private void buttonDone_IME_Click(object sender, EventArgs e)
         {
             DataService ds = new DataService();
+            try
+            {
+                textBoxResult_IME.Text = Convert.ToString(ds.Calculate(Convert.ToInt32(textBoxVarX_IME.Text)));
+            }
+            catch
+            {
+                MessageBox.Show("Введены неверные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
 
+        private void buttonHelp_IME_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 0 выполнил студент группы РПСб-23-1 Чебулаев Андрей Геннадьевич", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void textBoxVarX_IME_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && (e.KeyChar != ',') && (e.KeyChar != 8))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void pictureBoxFormula_IME_Click(object sender, EventArgs e)
+        {
 
         }
 
-        private void FormMain_Load(object sender, EventArgs e)
+        private void groupBoxCondition_IME_Enter(object sender, EventArgs e)
         {
 
         }
