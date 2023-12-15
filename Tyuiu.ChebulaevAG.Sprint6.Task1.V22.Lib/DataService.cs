4573894ecf59c074468100dbbc862df67571f49d
Tyuiu.ChebulaevAG@outlook.com <andrey.chebulaev@gmail.com>
2023-12-15 20:25:16 +0500

    Sprint6

---
+++ Tyuiu.ChebulaevAG.Sprint6.Task1.V22.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.ChebulaevAG.Sprint6.Task1.V22.Lib
+{
+    public class DataService : ISprint6Task1V22
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round((3 * x) + 2 - (((2 * x) - x) / (Math.Cos(x) + 1)), 2);
+                valueArray[count] = y;
+                count++;
+                if ((Math.Cos(x) + 1) == 0)
+                {
+                    y = 0;
+                    valueArray[count] = y;
+                    count++;
+                }
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.ChebulaevAG.Sprint6.Task1.V22.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ChebulaevAG.Sprint6.Task1.V22.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ChebulaevAG.Sprint6.Task1.V22.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("649e51d6-e76c-4a14-9bb0-28743bdf5faa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
