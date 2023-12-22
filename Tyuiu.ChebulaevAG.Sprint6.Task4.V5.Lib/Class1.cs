27b270da3ccc4017542325437f149ac4ec1a3c4e
AndreyChebulaev <144042671+AndreyChebulaev@users.noreply.github.com>
2023-12-22 16:38:12 +0500

    s6.cs

--- Tyuiu.ChebulaevAG.Sprint6.Task4.V5.Lib/Class1.cs
+++ Tyuiu.ChebulaevAG.Sprint6.Task4.V5.Lib/Class1.cs
@@ -1,12 +1,35 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint6;
 
 namespace Tyuiu.ChebulaevAG.Sprint6.Task4.V5.Lib
 {
-    public class Class1
+    public class DataService : ISprint6Task4V5
     {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round((2 * Math.Sin(x)) / (3 * x + 1.2) + Math.Cos(x) * 2, 2);
+                valueArray[count] = y;
+                count++;
+                if ((3 * x + 1.2) == 0)
+                {
+                    y = 0;
+                    valueArray[count] = y;
+                    count++;
+                }
+            }
+            return valueArray;
+        }
     }
 }
