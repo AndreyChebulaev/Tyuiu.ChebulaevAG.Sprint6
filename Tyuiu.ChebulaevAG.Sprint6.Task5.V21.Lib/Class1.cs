267be087322d9a746aa4840a829b0ae45362016c
AndreyChebulaev <144042671+AndreyChebulaev@users.noreply.github.com>
2023-12-22 16:49:53 +0500

    s6

--- Tyuiu.ChebulaevAG.Sprint6.Task5.V21.Lib/Class1.cs
+++ Tyuiu.ChebulaevAG.Sprint6.Task5.V21.Lib/Class1.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Tyuiu.ChebulaevAG.Sprint6.Task5.V21.Lib
 {
-    public class Class1
+    public class DataService : ISprint6Task5V21
     {
+        public int len = 0;
+        public double[] LoadFromDataFile(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null) { len++; }
+            }
+
+            double[] numsArray = new double[len];
+
+            int index = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    numsArray[index] = Convert.ToDouble(line);
+                    index++;
+                }
+            }
+
+            numsArray = numsArray.Where(val => val % 5 != 0).ToArray();
+
+            return numsArray;
+        }
     }
 }
