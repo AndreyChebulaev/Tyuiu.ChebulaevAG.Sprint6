60cb0ef5e6788868bb90d3a067de6ed2b892ea25
AndreyChebulaev <144042671+AndreyChebulaev@users.noreply.github.com>
2023-12-22 16:41:43 +0500

    s6.cs

--- Tyuiu.ChebulaevAG.Sprint6.Task6.V25.Lib/Class1.cs
+++ Tyuiu.ChebulaevAG.Sprint6.Task6.V25.Lib/Class1.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint6;
+using System.IO;
+using System.Text.RegularExpressions;
 
-namespace Tyuiu.ChebulaevAG.Sprint6.Task5.V25.Lib
+namespace Tyuiu.ChebulaevAG.Sprint6.Task6.V25.Lib
 {
-    public class Class1
+    public class DataService : ISprint6Task6V25
     {
+        public string CollectTextFromFile(string path)
+        {
+            string resStr = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line == "")
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        List<string> result = new List<string>();
+                        MatchCollection words = Regex.Matches(line, @"[А-Яа-яA-Za-z]+");
+                        foreach (var word in words)
+                        {
+                            result.Add(word.ToString());
+                        }
+
+                        foreach (var word in result)
+                        {
+                            if (word.Contains("g"))
+                            {
+                                resStr += word + " ";
+                            }
+                        }
+                    }
+                }
+            }
+            resStr = resStr.Trim();
+            return resStr;
+        }
     }
 }
