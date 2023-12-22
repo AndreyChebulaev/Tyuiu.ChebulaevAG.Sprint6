9d3d672e5ab5d2d765ca7076498afad1ad4ef3cd
AndreyChebulaev <144042671+AndreyChebulaev@users.noreply.github.com>
2023-12-22 16:42:24 +0500

    Update UnitTest1.cs

--- Tyuiu.ChebulaevAG.Sprint6.Task6.V25.Test/UnitTest1.cs
+++ Tyuiu.ChebulaevAG.Sprint6.Task6.V25.Test/UnitTest1.cs
@@ -1,14 +1,33 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using Tyuiu.ChebulaevAG.Sprint6.Task6.V25.Lib;
+using System.IO;
 
-namespace Tyuiu.ChebulaevAG.Sprint6.Task5.V25.Test
+namespace Tyuiu.ChebulaevAG.Sprint6.Task6.V25.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidCheckedPath()
         {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint6\InPutFileTask6V25.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+
+        [TestMethod]
+        public void ValidCollectTextFromFile()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint6\InPutFileTask6V25.txt";
+            string res = ds.CollectTextFromFile(path);
+            string wait = "nXkwQYzgZ XcNHugFmbsRgVVfsgHNGuUC";
+            Assert.AreEqual(wait, res);
         }
     }
 }
