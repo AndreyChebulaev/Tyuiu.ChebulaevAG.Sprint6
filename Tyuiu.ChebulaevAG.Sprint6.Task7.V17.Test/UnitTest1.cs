ad5b8dfc82f76288d03d74f46605dd40dd85082d
AndreyChebulaev <144042671+AndreyChebulaev@users.noreply.github.com>
2023-12-22 16:45:32 +0500

    s6

--- Tyuiu.ChebulaevAG.Sprint6.Task7.V17.Test/UnitTest1.cs
+++ Tyuiu.ChebulaevAG.Sprint6.Task7.V17.Test/UnitTest1.cs
@@ -1,14 +1,20 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using Tyuiu.ChebulaevAG.Sprint6.Task7.V17.Lib;
+using System.IO;
 
 namespace Tyuiu.ChebulaevAG.Sprint6.Task7.V17.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidGetMatrix()
         {
+            string path = @"C:\Users\acheb\source\repos\Tyuiu.ChebulaevAG.Sprint6\Tyuiu.ChebulaevAG.Sprint6.Task7.V17\bin\Debug\InPutFileTask7V17.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
         }
     }
 }
